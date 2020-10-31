e0a062c1da809a3612fc28352e5d57fa860c3884
vikaxx <victoria.aliaeva@gmail.com>
2020-10-31 14:15:52 +0200

    unit test

---
+++ src/test/java/ua/profitsoft/AccountantTest.java
@@ -0,0 +1,16 @@
+package ua.profitsoft;
+
+import junit.framework.TestCase;
+
+public class AccountantTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void tearDown() throws Exception {
+    }
+
+    public void testAccountSalary() {
+    }
+}
